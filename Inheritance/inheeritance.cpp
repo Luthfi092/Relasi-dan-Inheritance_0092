a5516281eff2309892a395547ac9e3daaada4a96
upi1682 <upi538287@gmail.com>
2025-05-27 16:17:20 +0700

    nambahin class turunan pelajar dari orang

--- Inheritance/inheeritance.cpp
+++ Inheritance/inheeritance.cpp
@@ -19,4 +19,18 @@ public :
         }
 };
 
+class pelajar : public orang {
+public :
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + "dari sekolah " + sekolah + "\n\n"
+    }
+};
 
